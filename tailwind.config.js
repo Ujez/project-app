2a0f45dd9b3faffc7bc412e4af6fa596c9b87a13
ujez <ujezujez@gmail.com>
2022-05-15 13:48:41 +0100

    added the hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,33 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "media",
   theme: {
     screens: {
-      sm: '640px',
+      sm: "640px",
       // => @media (min-width: 640px) { ... }
 
-      md: '768px',
+      md: "768px",
       // => @media (min-width: 768px) { ... }
 
-      lg: '1024px',
+      lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      xl: '1280px',
+      xl: "1280px",
       // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
+      "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      backgroundColor: {
+        primary: "var(--color-bg-primary)",
+        secondary: "var(--color-bg-secondary)",
+      },
+      textColor: {
+        accent: "var(--color-text-accent)",
+        primary: "var(--color-text-primary)",
+        secondary: "var(--color-text-secondary)",
+      },
     },
   },
   plugins: [],

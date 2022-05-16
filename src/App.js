e0f321c24130e7dc95d3b1eaa774ed1aed8337ef
ujez <ujezujez@gmail.com>
2022-05-16 13:24:14 +0100

    added the support component and modified the hero comp

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Support from "./components/Support";
+
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
       <About />
+      <Support />
 
     </>
   );
